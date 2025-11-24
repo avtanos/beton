b23f03d98d05d0e6f4df35454cf6d3dd4a026208
avtanos <avtanos@gmail.com>
2025-11-24 22:37:27 +0600

    first commit

--- Минтруда/РУПИО/rupio/build_check.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Checking TypeScript compilation...
-npx tsc --noEmit
-if %errorlevel% equ 0 (
-    echo TypeScript compilation successful!
-) else (
-    echo TypeScript compilation failed!
-)
-pause
